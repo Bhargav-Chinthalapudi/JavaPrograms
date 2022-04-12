c6bb56b56daedd48c0551953fc814993cfc734dc
Bhargav Chinthalapudi <bhargav.chinthalapudid6@gmail.com>
2022-04-12 15:01:00 +0530

    f2 added

--- src/JavaSessions/Feature.java
+++ src/JavaSessions/Feature.java
@@ -5,6 +5,7 @@ public class Feature {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("f1 added");
+		 System.out.println("f2 added");
 	}
 
 }
