e49cfb13b275c49ee9c697ed28ea79edf071a57c
Bhargav Chinthalapudi <bhargav.chinthalapudid6@gmail.com>
2022-04-12 19:23:22 +0530

    product demo code

---
+++ src/JavaSessions/Productdemo.java
@@ -0,0 +1,12 @@
+package JavaSessions;
+
+public class Productdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("product demo class");
+
+	}
+
+}
