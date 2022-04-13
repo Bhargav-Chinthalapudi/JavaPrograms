c659e2613b6170cbf65f06a4bc67f090396ee15e
Bhargav-Chinthalapudi <103302225+Bhargav-Chinthalapudi@users.noreply.github.com>
2022-04-13 19:39:55 +0530

    Update Sample.java

--- src/JavaSessions/Sample.java
+++ src/JavaSessions/Sample.java
@@ -1,3 +1,10 @@
-public class Domestifc {
-System.out.println("sample code");
+package JavaSessions;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sample class");
+	}
+
 }
